a34f1e549161624fdcfb7ce89fe6a3452d1cfc79
jusalanen <jussialanen9@gmail.com>
2019-08-23 22:27:43 +0300

    lisää tehtävä 4.2 ja logger

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controller/blogs')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(logger)
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
