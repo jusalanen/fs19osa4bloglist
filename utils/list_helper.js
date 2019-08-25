89f5ff6bf83061591507e71a393a2aca9e57c7c6
jusalanen <jussialanen9@gmail.com>
2019-08-25 18:45:58 +0300

    lisää tehtävä 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  blogsArr = [].concat(blogs)
+  let totalLikes = 0
+  blogsArr.forEach(blog => {
+    totalLikes += blog.likes
+  })
+  return totalLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
