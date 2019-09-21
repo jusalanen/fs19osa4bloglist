e9d3f8ea554505e5dd63ae3d477c8fa55b25d7f1
jusalanen <jussialanen9@gmail.com>
2019-09-21 19:36:39 +0300

    korjaa teht 4.10

--- controller/blogs.js
+++ controller/blogs.js
@@ -7,12 +7,28 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const newBlog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+  /*const blog = new Blog(request.body)
 
-  const savedBlog = await newBlog.save()
-  
-  response.status(201).json(savedBlog.toJSON())
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result.toJSON())
+    })*/
+  const body = request.body
+  const newBlog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes
+  })
+  try {
+    const savedBlog = await newBlog.save()
+    response.status(201).json(savedBlog.toJSON())
+  } catch (exception) {
+  //  console.log(exception)
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,8 +3,19 @@ const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
 const initBlogs = require('./test_helper')
+const config = require('../utils/config')
+const http = require('http')
+
+/*jest.setTimeout(15000)
+jest.retryTimes(3)
+const server = http.createServer(app)*/
 
 const api = supertest(app)
+/*beforeAll(async (done) => {
+  await server.listen(config.PORT, () => {console.log(`Server running on port ${config.PORT}`)})
+  api = supertest(server)
+  done()
+})*/
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -20,37 +31,57 @@ test('blogs are returned as json of correct size', async () => {
   const response = await api.get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-
-  expect(response.body.length).toBe(2)
+  console.log(response.body)
+  expect(response.body.length).toBe(initBlogs.length)
 })
 
-test('identifier is shown as "id"' , async () => {
+test('identifier is returned as "id"' , async () => {
   const response = await api.get('/api/blogs')
   const blogs = [].concat(response.body)
   blogs.forEach( blog => {
+    console.log(blog)
     expect(blog.id).toBeDefined()
   })
 })
 
 test('post increases the number of blogs by one', async () => {
-  const newBlog = new Blog(
-    {
-      title: 'New Blog',
-      author: 'New Author',
-      url: 'localhost:something/new',
-      likes: 0
-    }
-  )
-  try {
-    await api.post('api/blogs').send(newBlog)
-    .expect(201)
-    const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(initBlogs + 1)
-  } catch (error) {
-    console.log(error)
-  } 
+  const newBlog = {
+    title: 'New Blog',
+    author: 'New Author',
+    url: 'localhost:something/new',
+    likes: 0
+  }
+//try { 
+  await api.post('/api/blogs').send(newBlog)
+  .expect(201)
+  const response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(initBlogs.length + 1)
+  console.log(response)
+/*} catch (ex) {
+  console.log(ex.message)
+}*/
 })
 
+<<<<<<< Updated upstream
+=======
+test('likes set to zero if not set in the request', async () => {
+  const newBlog = {
+    title: 'New Blog',
+    author: 'New Author',
+    url: 'localhost:something/new'
+  }
+//try {
+  const savedBlog = await api.post('/api/blogs').send(newBlog)
+  if (savedBlog) {
+    expect(savedBlog.likes).toBe(0)
+//    console.log(savedBlog)
+  }
+/*} catch (ex) {
+  console.log(ex.message)
+}*/
+})
+
+>>>>>>> Stashed changes
 afterAll(() => {
   mongoose.connection.close()
 })
