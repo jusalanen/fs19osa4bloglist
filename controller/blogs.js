8b7a8038c96b95f2324319e84e5b3003a92d666f
jusalanen <jussialanen9@gmail.com>
2019-09-26 19:35:39 +0300

    lisää tehtävän 4.17 blog&user

--- controller/blogs.js
+++ controller/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
  
   response.json(blogs.map(blog => blog.toJSON()))
 })
@@ -12,11 +13,14 @@ blogsRouter.post('/', async (request, response, next) => {
   if (body.title === undefined || body.url === undefined) {
     return response.status(400).end()
   }
+  const users = await User.find({})
+  const userNr = Math.floor(Math.random() *2)
   const newBlog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes === undefined ? 0 : body.likes
+    likes: body.likes === undefined ? 0 : body.likes,
+    user: users[userNr]
   })
   try {
   const savedBlog = await newBlog.save()
